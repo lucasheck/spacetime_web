285c9306b61108b1dc1541fc81a654d6aabfbc37
lucasheck <lucasheck23@gmail.com>
2023-05-17 20:00:51 -0400

    1 - Creating the project with nextjs 2 - Installing all the packages 3 - Creating the first page

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="h-screen bg-zinc-900 p-6 text-zinc-50">
+        <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+      </div>
+    </div>
+  )
+}
