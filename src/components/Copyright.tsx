849377a5df8f4fd94072cba946f855a9f7371f09
lucasheck <lucasheck23@gmail.com>
2023-05-19 23:38:23 -0400

    1 - Installing axios 2 - Installing jwt-decode 3 - Spliting homepage in components 4 - Validanting authenticated user with cookie 5 - Signing in using backend authentication

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,12 @@
+export function Copyright() {
+  return (
+    <div>
+      <div className="text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da{' '}
+        <a className="underline hover:text-gray-100" href="#">
+          Rocketseat
+        </a>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import { getUser } from "@/lib/auth";
+import Image from "next/image";
+
+export function Profile() {
+  const { name, avatarUrl } = getUser();
+  return (
+    <div className="flex items-center gap-3 text-left transition-colors">
+      <Image
+        src={avatarUrl}
+        width={40}
+        height={40}
+        alt="userPhoto"
+        className="h-10 w-10 rounded-full"
+      />
+
+      <p className="max-w-[200px] text-sm leading-snug">
+        <span className="block">Olá, {name}</span>
+        <a href="" className="block text-red-400 hover:text-red-300">
+          Sair
+        </a>
+      </p>
+    </div>
+  );
+}
